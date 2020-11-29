12da615b745f4ebf79675fd09f1181c424a89c57
shrayal <71540442+shrayal@users.noreply.github.com>
2020-11-29 10:48:43 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,21 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.v=255
+  }
+display(){
+console.log(this.body.speed)
+if(this.body.speed<3){
+super.display()
+}
+else{
+World.remove(world,this.body)
+push()
+this.v=this.v-5
+tint(255,this.v)
+image(this.image,this.body.position.x,this.body.position.y,this.width,this.height)
+pop()
+}
+}
+};
